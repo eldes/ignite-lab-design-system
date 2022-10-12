d26dc686799094f0eb9c0fcdfbb141d7c30ec434
Eldes <studio@eldes.com>
2022-10-12 19:37:41 -0300

    Configure the tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+import './styles/global.css';
+
 const App: React.FC = function () {
   return (
-    <h1>Ignite Lab - Design System</h1>
+    <h1 className='font-bold text-5xl'>Ignite Lab - Design System</h1>
   );
 };
 
